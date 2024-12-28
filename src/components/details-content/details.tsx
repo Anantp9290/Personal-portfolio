ff7c98026e8ae475a4b5b71fad5c51d7fdb39ad2
Anantp9290 <anantoffice13@gmail.com>
2024-12-28 15:15:34 +0530

    fixes

--- src/components/details-content/details.tsx
+++ src/components/details-content/details.tsx
@@ -5,9 +5,9 @@ export default function Details() {
     <>
       <div className="flex h-full w-full">
         <div className="container mx-auto">
-          <div className="flex gap py-2">
+          <div className="flex gap-1 py-2">
             <Sidebar />
-            <div className="text-white flex-1">conetetworks</div>
+            <div className="text-white flex-1 pl-4">conetetworks</div>
           </div>
         </div>
       </div>
