6ab1b61a746851c94a911cfa361ae079e2dce143
Anantp9290 <anantoffice13@gmail.com>
2024-12-30 16:59:50 +0530

    varial declare in global file

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -3,19 +3,19 @@ export default function Navbar() {
     <>
       <header className="mt-6">
         <div className="container mx-auto">
-          <div className="flex justify-between items-center bg-[#fff] rounded p-5">
-            <img src="image" alt="logo" />
+          <div className="flex justify-between items-center bg-[var(--background)] rounded p-5">
+            {/* <img src="image" alt="logo" /> */}
+            <p>image</p>
 
             <button
               type="submit"
-              className="px-[20px] py-[10px] bg-[#1a0928] text-white max-w-fit w-fit relative overflow-hidden group transition-all duration-1000 ease-in-out hover:bg-[#fff] hover:text-[#1a0928]"
+              className="px-[20px] py-[10px] bg-[var(--foreground)] text-white max-w-fit w-fit relative overflow-hidden group transition-all duration-1000 ease-in-out hover:bg-transparent hover:text-[var(--foreground)]"
             >
-              <span className="absolute top-0 left-0 w-0 h-[2px] bg-transparent transition-all duration-1000 ease-in-out group-hover:w-full group-hover:bg-blue-900"></span>
-              <span className="absolute bottom-0 right-0 w-0 h-[2px] bg-transparent transition-all duration-1000 ease-in-out group-hover:w-full group-hover:bg-blue-900"></span>
-              <span className="absolute bottom-0 left-0 w-[2px] h-0 bg-transparent transition-all duration-1000 ease-in-out group-hover:h-full group-hover:bg-blue-900"></span>
-              <span className="absolute bottom-0 right-0 w-[2px] h-0 bg-transparent transition-all duration-1000 ease-in-out group-hover:h-full group-hover:bg-blue-900"></span>
-
-              <span className="relative z-10">Download CV</span>
+              <span className="absolute top-0 left-0 w-0 h-[2px] bg-transparent transition-all duration-1000 ease-in-out group-hover:w-full group-hover:bg-[var(--darkblue)]"></span>
+              <span className="absolute bottom-0 right-0 w-0 h-[2px] bg-transparent transition-all duration-1000 ease-in-out group-hover:w-full group-hover:bg-[var(--darkblue)]"></span>
+              <span className="absolute bottom-0 left-0 w-[2px] h-0 bg-transparent transition-all duration-1000 ease-in-out group-hover:h-full group-hover:bg-[var(--darkblue)]"></span>
+              <span className="absolute bottom-0 right-0 w-[2px] h-0 bg-transparent transition-all duration-1000 ease-in-out group-hover:h-full group-hover:bg-[var(--darkblue)]"></span>
+              <span className="relative z-1">Download CV</span>
             </button>
           </div>
         </div>
