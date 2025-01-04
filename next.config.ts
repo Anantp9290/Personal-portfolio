b02426201a5cefa75ac3d1c725dc2564d2ab863f
Anantp9290 <anantoffice13@gmail.com>
2025-01-04 22:06:42 +0530

    buile error resolve

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // images: {
-  //   domains: ["example.com"],
-  // },
+  images: {
+    domains: ['www.w3schools.com'], // Add the domain of your external image here
+  },
   // env: {
   //   API_URL: process.env.API_URL,
   // },
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Details from "@/components/details-content/details";
 import Navbar from "@/components/navbar/navbar";
-import Image from "next/image";
 
 export default function Home() {
   return (
