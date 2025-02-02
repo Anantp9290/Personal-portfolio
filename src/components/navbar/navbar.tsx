a2e7b8ead7431201e16bf435da6d178dc96ca46c
Anantp9290 <anantpatel543@gmail.com>
2025-02-02 09:59:04 +0530

    button isuue resolved

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -12,7 +12,7 @@ export default function Navbar() {
               className="px-[20px] py-[10px] bg-[var(--foreground)] text-white max-w-fit w-fit relative overflow-hidden group transition-all duration-1000 ease-in-out hover:bg-transparent hover:text-[var(--foreground)]"
             >
               <span className="absolute top-0 left-0 w-0 h-[2px] bg-transparent transition-all duration-1000 ease-in-out group-hover:w-full group-hover:bg-[var(--darkblue)]"></span>
-              <span className="absolute bottom-0 right-0 w-0 h-[2px] bg-transparent transition-all duration-1000 ease-in-out group-hover:w-full group-hover:bg-[var(--darkblue)]"></span>
+              <span className="absolute bottom-0 right-0 w-0 h-[2.5px] bg-transparent transition-all duration-1000 ease-in-out group-hover:w-full group-hover:bg-[var(--darkblue)]"></span>
               <span className="absolute bottom-0 left-0 w-[2px] h-0 bg-transparent transition-all duration-1000 ease-in-out group-hover:h-full group-hover:bg-[var(--darkblue)]"></span>
               <span className="absolute bottom-0 right-0 w-[2px] h-0 bg-transparent transition-all duration-1000 ease-in-out group-hover:h-full group-hover:bg-[var(--darkblue)]"></span>
               <span className="relative z-1">Download cv</span>
