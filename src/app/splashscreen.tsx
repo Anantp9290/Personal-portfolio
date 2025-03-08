400e54ef961734b9218a87b9e9c280a393867ccc
Anantp9290 <anantpatel543@gmail.com>
2025-03-08 13:04:19 +0530

    splash screen adde

---
+++ src/app/splashscreen.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+export default function SplashWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [showSplash, setShowSplash] = useState(true);
+  const [dots, setDots] = useState("");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDots((prev) => (prev.length < 3 ? prev + "." : ""));
+    }, 500);
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setShowSplash(false), 5000); // Show splash for 10 seconds
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <>
+      {showSplash ? (
+        <div className="flex items-center justify-center h-screen w-full bg-[#241f40] text-white text-3xl font-bold">
+          Loading{dots}
+        </div>
+      ) : (
+        <div id="root">{children}</div>
+      )}
+    </>
+  );
+}
